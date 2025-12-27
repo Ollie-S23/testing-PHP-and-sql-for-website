3268264ab87536734d681401fc8272b85675bd9f
Oliver Scott <olliechina1516@gmail.com>
2025-12-27 12:36:34 +1100

    image string data fetched displayed. Need convert

---
+++ testImageDisplay.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Test Image Display">
+    <meta name="author" content="Oliver Scott">
+    <title>Basic HTML Template</title>
+</head>
+<body>
+
+    <?php
+        include('database.php');
+
+        try {
+            $sql = "SELECT image_path FROM post_images WHERE id = 9";
+            $result = mysqli_query($conn, $sql); //execute query
+
+            //det actual daata from result
+            $row = mysqli_fetch_assoc($result);
+
+            echo $row['image_path']; //displays base64 string
+
+            
+
+        } catch (mysqli_sql_exception) {
+            echo "could not execute query. <br>";
+        }
+
+        mysqli_close($conn);
+    ?>
+
+
+</body>
+</html>
